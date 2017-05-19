5d807763d5b1ebdf3b360acc04e61a41f03c07d5
Daan Middendorp <github-d@landgenoot.com>
2017-05-19 13:03:55 +0200

    Add Savoir

--- create_elections.py
+++ create_elections.py
@@ -5,10 +5,16 @@ import csv
 import hashlib
 
 import base58
+from Savoir import Savoir
 
 # Update according to params.dat
 __version__ = ['00', '8c', 'b5', 'd6']
-__addressChecksum__ = '5afce7b2'
+__addresschecksum__ = '5afce7b2'
+__rpcuser__ = 'multichainrpc'
+__rpcpasswd__ = 'YoUrLoNgRpCpAsSwOrD'
+__rpchost__ = 'localhost'
+__rpcport__ = '22335'
+__chainname__ = 'myChain'
 
 def main():
     """
@@ -17,7 +23,6 @@ def main():
     parser = argparse.ArgumentParser(
         description='Create new elections and assign one token to every pubkey'
     )
-    parser.add_argument('--server', '-s')
     parser.add_argument('--token-name', '-n')
     parser.add_argument('--pubkeys', '-i')
 
@@ -27,41 +32,44 @@ def main():
         for row in csv.reader(csvfile, delimiter=' ', quotechar='|'):
             print pubkey_to_address(row[0])
 
+    api = Savoir(__rpcuser__, __rpcpasswd__, __rpchost__, __rpcport__, __chainname__)
+    api.getinfo()
+
 def pubkey_to_address(pubkey):
     """
     Returns a valid address on the blockchain for pubkey
     According to http://www.multichain.com/developers/address-key-format/
     """
-    # step 3
+    # Step 3
     pubkey_hash = hashlib.sha256(binascii.unhexlify(pubkey))
     ripemd160 = hashlib.new('ripemd160')
     ripemd160.update(pubkey_hash.digest())
 
-    # step 4
+    # Step 4
     pubkey160_hash = ripemd160.hexdigest()
 
-    # step 5
+    # Step 5
     pubkey160_hash_w_version = ''
     for i in range(4):
         pubkey160_hash_w_version += __version__[i] + pubkey160_hash[(i*10):(i*10)+10]
 
-    # step 6
+    # Step 6
     sha256_of_160hash = hashlib.sha256(binascii.unhexlify(pubkey160_hash_w_version))
     sha256_of_160hash.hexdigest()
 
-    # step 7
+    # Step 7
     sha256_of_prev_sha256 = hashlib.sha256(sha256_of_160hash.digest())
 
-    # step 8
+    # Step 8
     checksum = sha256_of_prev_sha256.hexdigest()[0:8]
 
-    # step 9
-    xor_checksum = '{:08x}'.format(int(int(checksum, 16) ^ int(__addressChecksum__, 16)))
+    # Step 9
+    xor_checksum = '{:08x}'.format(int(int(checksum, 16) ^ int(__addresschecksum__, 16)))
 
-    # step 10
+    # Step 10
     binary_address = pubkey160_hash_w_version + xor_checksum
 
-    # step 11
+    # Step 11
     return base58.b58encode(binascii.unhexlify(binary_address))
 
 if __name__ == '__main__':
