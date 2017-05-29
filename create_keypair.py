093b13c03d55be93a64b4bd55b35b64a2ec21e4c
Daan Middendorp <github-d@landgenoot.com>
2017-05-29 12:29:16 +0200

    Create new passport generator untility

---
+++ create_keypair.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+This module creates a keypair which can be used to
+simulate a machine readable travel document.
+"""
+
+__author__ = "Daan Middendorp"
+__copyright__ = "Copyright 2017, Digital Voting Pass"
+__credits__ = ["Wilko Meijer", "Daan Middendorp", "Jonathan Raes", "Rico Tubbing"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+
+def main():
+    1+1
+
+
+if __name__ == '__main__':
+    main()
